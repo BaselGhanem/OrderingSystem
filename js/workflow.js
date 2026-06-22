b3689660770fedccbb53dda145d56b417c4d3004
Basel Ghanem <baselghanem21@gmail.com>
2026-06-22 14:37:19 +0300

    Add files via upload

--- js/workflow.js
+++ js/workflow.js
@@ -45,7 +45,7 @@ const state = {
     ordersStaffTab: 'approved'
 };
 
-const WORKFLOW_CACHE_VERSION = '20260622_market_popup_actions1';
+const WORKFLOW_CACHE_VERSION = '20260622_settings_bulk_products1';
 const CACHE_MAX_AGE_MS = 1000 * 60 * 60 * 12;
 const PAGE_CACHE_KEY = `dad_orders_${WORKFLOW_CACHE_VERSION}_${WORKFLOW_PAGE || 'workflow'}`;
 const ALL_ORDERS_CACHE_KEY = `dad_orders_${WORKFLOW_CACHE_VERSION}_orders_staff_all`;
